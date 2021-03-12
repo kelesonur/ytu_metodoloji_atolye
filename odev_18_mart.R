d3744e2f6b3f75dceb06d9e23e7dfa7c6aabee5f
Onur Keleş <onurkeles.boun@gmail.com>
2021-03-12 23:09:47 +0300

    Uploaded notes and homework.

---
+++ odev_18_mart.R
@@ -0,0 +1,27 @@
+# Ödev
+
+# 1'den 15 çýkarýn.
+
+
+# "benim_vektorum" adýnda bir vektör oluþturun ve içerisine 1'den 10'a kadar olan sayýlar yerleþtirin.
+
+
+# oluþturduðunuz vektöre 5 ekleyin.
+
+
+# benim_vektorum^10 ifadesinde 50 sayýsýndan küçük hangi sayýlar var? Mantýk operatörü kullanarak gösterin.
+
+
+# "z" adýnda baþka bir vektör oluþturun ve içerisine 15, 18, 30 sayýlarýný koyun.
+
+
+# "letters" adýndaki hazýr gelen vektörü kullanarak içindeki alfabenin 6. 7. ve 8. harflerini çekin.
+
+
+# "df" adýnda A ve B sütunlarýný içeren bir veri çerçevesi oluþturun. A sütunu 1'den 5'e kadar sayýlar içersin. 
+# B sütunu alfabenin ilk beþ harfini içersin.
+
+
+# library fonksiyonu ile ggplot2 paketini çaðýrýn. "df" veri çerçevesini kullanarak bir bar/sütun grafiði oluþturun.
+# A sütunu X ekseninde, B sütunu Y ekseninde olsun.
+
